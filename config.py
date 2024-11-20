7afd8de4224bee534b48e01773f2e5b20180a131
Starboymusic <visionm59@gmail.com>
2024-11-20 15:42:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ API_ID = int(environ.get("API_ID", "19193584"))
 API_HASH = environ.get("API_HASH", "6cce5fd44ffbeba47414ca91143dc8c2")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://envs.sh/59_.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '928411292').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "") # without @
 PORT = environ.get("PORT", "8080")
